28568153f6b8d85d0e9a408c1e6a4db6d0e5d0cd
shipzydev <shipzyservices@gmail.com>
2022-01-08 17:03:25 +0530

    upadate to test session

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
     //function to load data here from adminuser Api
-    $('#mydata').load("https://shipzy.xyz/Src/Admin/adminpage.html"); 
+    $('#mydata').load("https://shipzy.xyz/Src/Admin/adminpage.html");
+    $('#btnUser').click(function(){
+        var email=$('username').val();
+        var password=$('password').val();
+        var arr={
+            username:email,
+            password:password
+        }
+        var dataj=JSON.stringify(arr);
+        $.ajax({
+            url:"https://shipzy.site/api/adminapi.php",
+            data:dataj,
+            type:POST,
+            success:function(data)
+            {
+                $(#mydata).html(JSON.stringify(data));
+            }
+        });
+    });
+
+    // btnadmin call that and find session
+    $('#btnAdmin').click(function(){
+        $('#mydata').load('https://shipzy.site/other/test.php');
+    });
 });
