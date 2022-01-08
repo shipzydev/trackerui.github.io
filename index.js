0d1875158664f531c20ae460aa7d9caf0c6d7b84
shipzydev <shipzyservices@gmail.com>
2022-01-08 16:37:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
     //function to load data here from adminuser Api
-    $('#mydata').load("adminpage.html"); 
+    $('#mydata').load("src/admin/adminpage.html"); 
 });
