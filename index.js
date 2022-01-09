b466d0df431dee12b138e1d2e41d846815de3a41
shipzy services <shipzyservices@gmail.com>
2022-01-09 14:12:53 +0000

    Changes in eye icon
    eye issue solved

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 $(document).ready(function() {
     //function to load data here from adminuser Api
    // $('#mydata').load("https://shipzy.xyz/Src/Admin/adminpage.html");
+   
+   
+   // password hide show function
+   $('#eye').click(function()
+   {
+      var pass1= $('#password').attr('type');
+      if(pass1=='password')
+      {
+        $('#eye').text('visibility_on');
+        $('#eye').css("width","24px");
+        $('#password').attr('type','text ');
+
+      }
+      else
+      {
+        $('#eye').text('visibility_off');
+        $('#password').attr('type','password');
+      }
+       
+   })
+
+   // data retriever
+
     $('#btnUser').click(function(){
         var email=$('username').val();
         var password=$('password').val();
